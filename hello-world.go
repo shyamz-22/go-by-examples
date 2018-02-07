fac9f71f55f4715a0887beea806c95c5e2311c75
shyamz-22 <shykart2203@gmail.com>
2018-02-07 19:27:33 +0100

    Packages how tos

--- hello-world.go
+++ hello-world.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-by-examples/maths"
+	"github.com/go-by-examples/arithmetic"
+)
 
+// BODMAS
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(maths.AlgebraAPlusBFormula(12, 33))
+	fmt.Println(maths.AlgebraAMinusBFormula(12, 33))
+	fmt.Println(arithmetic.Compute(12, 33))
 }
+
