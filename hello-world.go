4fe8e7c4bbd86e9c910e22f6a6b56aa7b44b4044
shyamz-22 <shykart2203@gmail.com>
2018-02-11 13:11:16 +0100

    Variadic fns and for loop

--- hello-world.go
+++ hello-world.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"github.com/go-by-examples/maths"
@@ -8,8 +9,34 @@ import (
 // BODMAS
 func main() {
 	fmt.Println("hello world")
-	fmt.Println(maths.AlgebraAPlusBFormula(12, 33))
-	fmt.Println(maths.AlgebraAMinusBFormula(12, 33))
-	fmt.Println(arithmetic.Compute(12, 33))
-	fmt.Println(Algebraaplusbplusc(1, 2, 3))
+	var number12 = 12
+
+	fmt.Println(maths.AlgebraAPlusBFormula(number12, 33))
+	fmt.Println(maths.AlgebraAMinusBFormula(number12, 33))
+	fmt.Println(arithmetic.Compute(number12, 33))
+
+	number12 = 13
+
+	//fmt.Println(Algebraaplusbplusc(1, 2, 3))
+
+	printMe("Sai")
+	printMe("Karthik")
+	printMe("Shyam")
+
+	fmt.Println("########################")
+
+	printAll("Sai", "Shyam", "Karthik", "Uma", "Umamaheswaran", "Pushpa")
+}
+
+func printAll(names ...string) {
+	for index, name := range names {
+		fmt.Println(fmt.Sprintf("At index %d is the name %s", index, name))
+		printMe(name)
 	}
+}
+
+func printMe(name string) {
+	fmt.Println(name + " is learning go lang")
+	fmt.Println(name + " is watching this is us")
+	fmt.Println(name + " is sharing his screen")
+}
