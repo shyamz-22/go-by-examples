1e420ea91c046d8569628c2eab1dd9a704109426
venkateswaran <venkat.star92@gmail.com>
2018-02-12 23:07:06 +0530

    variables

--- hello-world.go
+++ hello-world.go
@@ -1,42 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"github.com/go-by-examples/maths"
-	"github.com/go-by-examples/arithmetic"
-)
-
-// BODMAS
-func main() {
-	fmt.Println("hello world")
-	var number12 = 12
-
-	fmt.Println(maths.AlgebraAPlusBFormula(number12, 33))
-	fmt.Println(maths.AlgebraAMinusBFormula(number12, 33))
-	fmt.Println(arithmetic.Compute(number12, 33))
-
-	number12 = 13
-
-	//fmt.Println(Algebraaplusbplusc(1, 2, 3))
-
-	printMe("Sai")
-	printMe("Karthik")
-	printMe("Shyam")
-
-	fmt.Println("########################")
-
-	printAll("Sai", "Shyam", "Karthik", "Uma", "Umamaheswaran", "Pushpa")
-}
-
-func printAll(names ...string) {
-	for index, name := range names {
-		fmt.Println(fmt.Sprintf("At index %d is the name %s", index, name))
-		printMe(name)
-	}
-}
-
-func printMe(name string) {
-	fmt.Println(name + " is learning go lang")
-	fmt.Println(name + " is watching this is us")
-	fmt.Println(name + " is sharing his screen")
+import "fmt"
+func main(){
+	var a = "venkat is watching soccer"
+	var b = "what do we think of tottenham"
+	var c = "shit"
+	var d = "what do we think of shit"
+	var e = "Tottenham"
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
 }
--- maths.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func Algebraaplusbplusc(a, b, c int) int {
-	return (a * a * a) + (b * b * b) + (c * c * c) + 2*(a*b+b*c+c*a)
-}
